2dfb8b443425bbc5609d5a8f93876b0e65c1679e
Slobodan Stojkovic <sc.slobodanstojkovic@gmail.com>
2021-12-02 22:19:15 +0100

    First commit

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,18 @@
+import classes from "./Header.module.css";
+import cakesShelf from "../../assets/cakesShelf.jpg";
+import { HeaderCartButton } from "./HeaderCartButton";
+
+export const Header = (props) => {
+  return (
+    <>
+      <header className={classes.header}>
+        <h1>Sweet pleasures</h1>
+        <HeaderCartButton />
+      </header>
+      <div className={classes["main-image"]}>
+        {/* because it has dash inside of it we need to access it like this */}
+        <img src={cakesShelf} alt="A shelf full of delicious cakes!" />
+      </div>
+    </>
+  );
+};
